1e089b8f5c75252a87b96eb6d52ebd7db918bb05
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2022-11-16 02:03:24 +0300

    preload

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,3 +4,15 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
+window.onload = function() {
+
+    setTimeout(function() {
+        let preloader = document.getElementById("preloader_malc")
+        if (preloader) {
+            document.getElementById("preloader_malc").style.display = "none";
+        }
+        
+
+    }, 400);
+
+};
